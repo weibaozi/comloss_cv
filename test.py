f9ebf17637c52dd6d62cc6b362e86ed7291debf5
weibaozi <guo361270936@gmail.com>
2024-12-13 16:02:36 -0500

    add test.py

--- test.py
+++ test.py
@@ -1,31 +1,39 @@
-import wandb
-import random
+#!/usr/bin/env python
 
-# start a new wandb run to track this script
-wandb.init(
-    # set the wandb project where this run will be logged
-    project="my-awesome-project",
-    name="test",
+import rospy
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+import cv2
+from model import myModel
+def image_callback(msg,model):
+    """Callback function to process the image."""
+    bridge = CvBridge()
+    try:
+        # Convert the ROS Image message to a CV image
+        cv_image = bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
+        #print image shape
+        output = model.predict(cv_image)
+        print(output.sigmoid())
+        # Display the image
+        # cv2.imshow("Image Subscriber", cv_image)
+        # cv2.waitKey(1)
+    except CvBridgeError as e:
+        rospy.logerr(f"Error converting ROS Image to OpenCV: {e}")
 
-    # track hyperparameters and run metadata
-    config={
-    "task_name": "test",
-    "learning_rate": 0.02,
-    "architecture": "CNN",
-    "dataset": "CIFAR-100",
-    "epochs": 10,
-    }
-)
+def main():
+    model_path = 'model.pth'
+    mymodel=myModel(model_path, 'cpu')
+    rospy.init_node('image_subscriber', anonymous=True)
+    
+    # Subscribe to the image topic (replace '/camera/image' with your topic)
+    image_topic = "/jhu_daVinci/left/image_rect_color"
+    rospy.Subscriber(image_topic, Image, lambda msg: image_callback(msg, mymodel))
 
-# simulate training
-epochs = 10
-offset = random.random() / 5
-for epoch in range(2, epochs):
-    acc = 1 - 2 ** -epoch - random.random() / epoch - offset
-    loss = 2 ** -epoch + random.random() / epoch + offset
+    # Keep the node running
+    rospy.spin()
 
-    # log metrics to wandb
-    wandb.log({"acc": acc, "loss": loss})
+    # Cleanup
+    cv2.destroyAllWindows()
 
-# [optional] finish the wandb run, necessary in notebooks
-wandb.finish()
+if __name__ == '__main__':
+    main()
